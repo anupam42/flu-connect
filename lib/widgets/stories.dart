b4683a4534743d9f4faa6082f9b2837169adf10c
Anupam <anupam.udupi@gmail.com>
2025-01-08 17:24:57 +0530

    Refactor Stories widget; group stories by UID and enhance viewing functionality

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../screens/stories_screen.dart';
 import '../utils/colors.dart';
@@ -13,61 +14,132 @@ class Stories extends StatelessWidget {
     return Align(
       alignment: Alignment.centerLeft,
       child: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('stories').snapshots(),
-          builder: (context,
-              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Container();
+        stream: FirebaseFirestore.instance.collection('stories').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container();
+          }
+
+          // Group stories by UID
+          final groupedStories = <String, List<Map<String, dynamic>>>{};
+          for (var doc in snapshot.data!.docs) {
+            final story = doc.data();
+            final uid = story['uid'];
+            if (!groupedStories.containsKey(uid)) {
+              groupedStories[uid] = [];
             }
-            return ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: true,
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                return StoriesCard(snap: snapshot.data!.docs[index].data());
-              },
-            );
-          }),
+            groupedStories[uid]!.add(story);
+          }
+
+          return ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            shrinkWrap: true,
+            itemCount: groupedStories.keys.length + 1, // +1 for "Connect"
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                // Default circular with "Connect"
+                return GestureDetector(
+                  onTap: () {
+                    // Add your connect functionality here
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 3,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey.shade300,
+                            radius: 30,
+                            child: Text(
+                              'Connect',
+                              style: GoogleFonts.meowScript(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              // Stories for a particular UID
+              final uid = groupedStories.keys.elementAt(index - 1);
+              final stories = groupedStories[uid]!;
+
+              return StoriesCardGroup(stories: stories);
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-class StoriesCard extends StatelessWidget {
-  const StoriesCard({
-    super.key,
-    required this.snap,
-  });
+class StoriesCardGroup extends StatefulWidget {
+  const StoriesCardGroup({super.key, required this.stories});
 
-  final dynamic snap;
+  final List<Map<String, dynamic>> stories;
+
+  @override
+  State<StoriesCardGroup> createState() => _StoriesCardGroupState();
+}
+
+class _StoriesCardGroupState extends State<StoriesCardGroup> {
+  final Set<String> _viewedStories = {};
+
+  void _markAsViewed(String uid) {
+    setState(() {
+      _viewedStories.add(uid);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final firstStory = widget.stories[0];
+    final uid = firstStory['uid'];
+    final isViewed = _viewedStories.contains(uid);
+
     return GestureDetector(
       onTap: () {
         Navigator.of(context).push(
           PageAnimation.createRoute(
-              page: StoriesScreen(
-                username: snap['username'],
-                profPic: snap['userimage'],
-                uid: snap['uid'],
-              ),
-              beginOffset1: 0.0,
-              beginOffset2: 1.0),
+            page: StoriesScreen(
+              username: firstStory['username'],
+              profPic: firstStory['userimage'],
+              uid: uid,
+            ),
+            beginOffset1: 0.0,
+            beginOffset2: 1.0,
+          ),
         );
+        _markAsViewed(uid);
       },
       child: Container(
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           border: Border.all(
-            color: const Color.fromARGB(255, 237, 101, 232),
+            color: isViewed
+                ? Colors.transparent
+                : const Color.fromARGB(255, 237, 101, 232),
             width: 3,
           ),
         ),
         child: Padding(
           padding: const EdgeInsets.all(2.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage(snap['userimage']),
+            backgroundImage: NetworkImage(firstStory['userimage']),
             backgroundColor: secondaryColor,
             radius: 40,
           ),
