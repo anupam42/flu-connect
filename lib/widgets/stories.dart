81a42ff523c3cfecf3a06fdd9e45ade9212483d5
Anupam <anupam.udupi@gmail.com>
2025-01-22 20:26:07 +0530

    Remove unnecessary blank line in stories widget file

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../screens/stories_screen.dart';
 import '../utils/page_animation.dart';
 
